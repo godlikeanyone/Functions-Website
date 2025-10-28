71ebd1b0e1d5b2f9c62ef78e3360ca3a452acac3
Godlike <26649232+godlikeanyone@users.noreply.github.com>
2025-10-28 21:29:03 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -294,7 +294,7 @@ export default function Home() {
       
                 {/* Content */}
                 <motion.div className="relative p-6 md:p-8 z-10 flex flex-col justify-end h-full"
-                  style={{ maxWidth: "600px" }}
+                  style={{ maxWidth: "550px" }}
                   animate={{
                     justifyContent: active === index ? "flex-start" : "flex-end",
                     transition: { duration: 0.5 },
